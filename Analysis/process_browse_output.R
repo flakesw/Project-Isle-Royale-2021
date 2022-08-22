5a66fd6de993a1807b225f704a5d201acec5f32e
Sam Flake <33131088+flakesw@users.noreply.github.com>
2022-08-22 15:33:08 -0400

    Update process_browse_output.R

--- Analysis/process_browse_output.R
+++ Analysis/process_browse_output.R
@@ -75,6 +75,9 @@ browse_summaries2 <- browse_summaries %>%
 # Figures
 #-------------------------------------------------------------------------------
 
+browse_summaries_melt <- pivot_longer(browse_summaries2, cols = TotalPopulation:TotalK,
+                                      names_to = "Variable")
+browse_summaries_melt
 #AGB over time
 
 moosepop <- ggplot(data = browse_summaries2, mapping = aes(x = Time+2020, y = TotalPopulation)) + 
@@ -87,14 +90,15 @@ plot(moosepop)
 ggsave(file="moosepop.svg", plot=moosepop, width=5, height=4)
 
 
-mooseK <- ggplot(data = browse_summaries2, mapping = aes(x = Time+2020, y = TotalK)) + 
+mooseK <- ggplot(data = browse_summaries_melt[browse_summaries_melt$Variable %in% c("TotalPopulation","TotalK"),], 
+                 mapping = aes(x = Time+2020, y = value)) + 
   geom_point(aes(colour = climate, shape = climate)) + 
-  labs(title = "Total Moose Population",
+  labs(title = "Total Moose Population and Carrying Capacity",
        subtitle = "by browse scenario and climate scenario",
-       y = "Total Moose Population", x = "Simulation Year") + 
-  geom_smooth(aes(linetype = climate, colour = climate)) + 
-  geom_smooth(aes(x = Time + 2020, y = TotalPopulation, linetype = climate, colour = climate))
+       y = "Number of moose", x = "Simulation Year") + 
+  geom_smooth(aes(colour = climate, linetype = Variable))
 plot(mooseK)
+ggsave(file="mooseK.svg", plot=mooseK, width=5, height=4)
 
 ggplot(data = browse_summaries2, mapping = aes(x = Time, y = AverageForage)) + 
   geom_point(aes(colour = climate, shape = climate)) + 
